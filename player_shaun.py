8c4017018e5fa17c08e4ec9b813db3ccdade86fd
Richard Byrne <richard.login@gmail.com>
2020-10-09 15:25:53 +0100

    Prepared Player for GIFs

--- player_shaun.py
+++ player_shaun.py
@@ -1,6 +1,53 @@
 import turtle
 
-class player_shaun(object):
-    """description of class"""
+class PlayerShaun(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+
+        turtle.register_shape("Resources\\ShaunAnimation\\Shaun_Start.gif")
+
+
+
+
+        self.x_cor=0
+        self.y_cor=0
+        self.shape("Resources\\ShaunAnimation\\Shaun_Start.gif")
+        self.penup()
+        self.speed(0)
+        self.direction="go_down"
+        self.goto(self.x_cor,self.y_cor)
+
+
+    #Define Player Movement
+    def go_up(self):
+        move_to_x = self.xcor()
+        move_to_y = self.ycor()+24
+        self.goto(move_to_x,move_to_y)
+        self.shape("Resources\\Shaun.gif")
+        self.direction="go_up"
+
+
+    def go_down(self):
+        move_to_x = self.xcor()
+        move_to_y = self.ycor()-24
+        self.goto(move_to_x,move_to_y)
+        self.shape("Resources\\Shaun.gif")
+        self.direction="go_down"
+
+
+    def go_left(self):
+        move_to_x = self.xcor()-24
+        move_to_y = self.ycor()
+        self.goto(move_to_x,move_to_y)
+        self.shape("Resources\\Left_Facing_Shaun.gif")
+        self.direction="go_left"
+
+    def go_right(self):
+        move_to_x = self.xcor()+24
+        move_to_y = self.ycor()
+        self.goto(move_to_x,move_to_y)
+        self.shape("Resources\\Right_Facing_Shaun.gif")
+        self.direction="go_right"
+
 
 
