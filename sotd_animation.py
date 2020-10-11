924504fc188ff8e3bd768668ecf73651114b6787
Holiday Ansar-Robins <holiday.ansarrobins@gmail.com>
2020-10-11 16:29:55 +0100

    Animated walking for rest of directions

--- sotd_animation.py
+++ sotd_animation.py
@@ -25,10 +25,10 @@ def quit_game():
 
 #Listen to keyboard input
 turtle.listen()
-#turtle.onkey(self.player.go_left,"a")
+turtle.onkey(player.go_left,"a")
 turtle.onkey(player.go_right,'d')
-#turtle.onkey(self.player.go_up,'w')
-#turtle.onkey(self.player.go_down,'s')
+turtle.onkey(player.go_up,'w')
+turtle.onkey(player.go_down,'s')
 #turtle.onkey(self.fire_bullet,'f')
 turtle.onkey(quit_game,'q')
 
